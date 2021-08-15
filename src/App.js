363e0cc3a3dc3a5cd340f6a7692f093fc28b27f8
roshan Dahal <roshandahal747@gmail.com>
2021-08-15 07:42:16 -0600

    Server data fetched

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import './App.css';
-import React, { Component } from 'react'
+import "./App.css";
+import React, { Component } from "react";
+
+const API_URL = "https://5dfmr.sse.codesandbox.io/cryptos";
 
 export class App extends Component {
+    state = {
+        coins : {}
+    };
+
+    componentDidMount(){
+        fetch(API_URL)
+            .then((x) => x.json())
+            .then((coins) => {
+              this.setState({ coins: coins});
+            });
+        }
+        renderCryptoPrices(){
+            return Object.entries(this.state.coins).map((cryptoData) => (
+                <li key={cryptoData.toString()}>
+                    {cryptoData[0]}: {cryptoData[1]}
+                </li>
+            ));
+        }
+        
+        renderOptions(){
+            return Object.keys(this.state.coins).map( (key) => (
+                <option value={key}>{key}</option>
+            ));
+        }
     render() {
-        return (
-            <div>
-                
+        return(
+            <div className="App">
+                <h1>Paper trading app</h1>
+                <p>Balance : $10000</p>
+                <h2>Cryptos</h2>
+                <ul>{this.renderCryptoPrices()}</ul>
             </div>
         )
     }
 }
 
-export default App
+export default App;
 
