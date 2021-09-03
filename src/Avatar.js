499d6179ce46fb8298fda488da43867674a5539e
roshan Dahal <roshandahal747@gmail.com>
2021-09-03 16:34:46 -0600

    Avatar added

---
+++ src/Avatar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import "./Avatar.css"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1)
+    }
+  }
+}));
+
+export default function ImageAvatars() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Avatar alt="Remy Sharp" src="https://media-exp1.licdn.com/dms/image/C5603AQGlJfPN0Wu88w/profile-displayphoto-shrink_400_400/0/1627602129072?e=1635984000&v=beta&t=O99s6JoeCNEA6l7-azw37DO30e3WIWsfXJqstKlImvI" />
+    </div>
+  );
+}
