47620af03dc96dd1dda8694952f3672828f34fbd
roshan Dahal <roshandahal747@gmail.com>
2021-09-03 16:35:45 -0600

    Remaining balance functionality added

---
+++ src/RemainingBalance.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './RemainingBalance.css';
+
+export default class Investing extends Component {
+    state = {
+        balance : {}
+    };
+    componentDidMount(){
+        fetch("https://i3g96.sse.codesandbox.io/balance")
+        .then(response => response.json())
+        .then(( balance ) => {
+            this.setState({ balance :  balance});
+            console.log("REMAING BALANCE",balance);  
+        });
+        
+    }
+    
+    renderBalance(){
+		return Object.values(this.state.balance).map((myBalance) => {
+			return (
+				<li key={myBalance.value}>
+					<h5>Welcome !{myBalance.name}</h5>
+					<h5>Remaining balance:  ${myBalance.amount.toFixed()}</h5>
+				</li>
+			)
+		})
+	}
+    render() {
+        return (
+            <div>
+            <ul>{this.renderBalance()}</ul>
+            </div>
+        )
+    }
+}
